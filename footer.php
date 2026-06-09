29af8fd154ee9c8c7dd3b2648cd38a18b9bd4004
Lucas Veiga <lucas@lucasveiga.com>
2026-06-08 21:58:25 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,18 +6,11 @@
 ?>
 
   <footer>
-      <p>
-        <?php bloginfo('name'); ?> is proudly powered by
-        <a href="http://wordpress.org/">WordPress</a>, and built using the <a href="http://html5boilerplate.com/">HTML5 Boilerplate</a>.
-        <br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-        and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
-        <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-      </p>
+     
   </footer>
 </div> <!--! end of #container -->
 
 
-  <?php versioned_javascript($GLOBALS["TEMPLATE_RELATIVE_URL"]."bower_components/jquery/dist/jquery.min.js") ?>
   <?php versioned_javascript($GLOBALS["TEMPLATE_RELATIVE_URL"]."js/functions.js") ?>
   <?php wp_footer(); ?>
 
